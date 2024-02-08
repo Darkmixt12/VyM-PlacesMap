a79dadbda05f2869b292592d379ce489b42d51c5
Steven Muñoz <darkmixt@gmail.com>
2024-02-08 14:37:24 -0600

    feat: mapbox map added

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Map} from 'mapbox-gl';
+import { PlacesService } from './../../services/';
+import { AfterViewInit, Component, ElementRef, ViewChild, inject } from '@angular/core';
 
 @Component({
   selector: 'app-map-view',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
 
+  private placesService = inject(PlacesService);
+
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
+
+  ngAfterViewInit(): void {
+
+    if(!this.placesService.userLocation) throw Error(' No existe user location')
+
+    const map = new Map({
+      container: this.mapDivElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.placesService.userLocation, // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+    console.log(this.placesService.userLocation)
+      
+  }
+
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoidmljaW91cy10cnVlcyIsImEiOiJjbHNkanN6eTQxMGhjMmtvN2I5NnFhZndlIn0.7dL82yc4vCcImtHvGOZkfQ';
+
 if (!navigator.geolocation) {
   alert('Navegador no soporta la Geolocation')
   throw new Error('Navegador no soporta la Geolocation')

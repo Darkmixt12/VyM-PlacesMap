c8867154d233afc441a6fee7cc9dba858167c024
Steven Muñoz <darkmixt@gmail.com>
2024-02-08 10:25:53 -0600

    feat new map project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'map', 
+		loadComponent: () => import('./maps/layout/maps-layout/maps-layout.component'),
+		
+		children: [
+			{
+				path: 'view',
+				title: 'Puntos de Compra',
+				loadComponent: () => import('./maps/pages/maps-page/maps-page.component'),
+			},
+			{
+				path: '', redirectTo: '/map/view', pathMatch: 'full'
+			}
+		]
+	  },
+	  {
+		path: '',
+		redirectTo: '/map/view',
+		pathMatch: 'full'
+	  }
+];
---
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-maps-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './maps-layout.component.html',
+  styleUrl: './maps-layout.component.css'
+})
+export default class MapsLayoutComponent {
+
+}
